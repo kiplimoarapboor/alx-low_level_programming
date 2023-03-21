acdf4596614d4cc3a6ccb8de10f52b32889f582b
kiplimoboor <kiplimoboor@hotmail.com>
2023-03-21 06:55:19 -0700

    checks if lower or upper

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _islower- checks if character is lower or upper
+ *
+ * @c: character keyed in
+ *
+ * Return: 1 is lower, 0 otherwise
+ */
+
+int _islower(int c)
+{
+	if ((c >= 97) && (c <= 122))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
