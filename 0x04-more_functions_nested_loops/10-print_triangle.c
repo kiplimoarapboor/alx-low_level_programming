df2e0a7574d7421cdc17030d0015350840ee3cdb
kiplimoboor <kiplimoboor@hotmail.com>
2023-03-23 10:55:24 -0700

    prints a right angled triangle

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -22,9 +22,9 @@ void print_triangle(int size)
 			_putchar(' ');
 		}
 		for (j = 0; j <= i; j++)
-        {
-            _putchar('#');
-        }
+		{
+			_putchar('#');
+		}
 		_putchar('\n');
 	}
 }
