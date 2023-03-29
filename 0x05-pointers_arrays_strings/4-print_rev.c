9bc1da9a900a6cef8c4fc0e330b7809de9377520
kiplimoboor <kiplimoboor@hotmail.com>
2023-03-29 03:40:34 -0700

    prints a string in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_rev- prints string in reverse
+ *
+ * @s: string to be rversed
+ */
+
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+
+	while (i >= 0)
+	{
+		_putchar(s[i]);
+		i--;
+	}
+	_putchar('\n');
+}
