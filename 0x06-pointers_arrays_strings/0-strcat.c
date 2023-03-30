729593bb5cdfd4d86cfe928c42846230fedd5843
kiplimoboor <kiplimoboor@hotmail.com>
2023-03-30 15:05:29 +0000

    created the main.h file

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,13 @@
+/**
+ * _strcat- concatenates two strings
+ *
+ * @dest: the destination address
+ * @src: the source address
+ *
+ * Return: returns a concatenation of two strings
+ */
+
+char *_strcat(char *dest, char *src)
+{
+
+}
