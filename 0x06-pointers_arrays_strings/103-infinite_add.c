7adf2846f8a2aaa91f491b8babc6b3c929c40dd6
kiplimoboor <kiplimoboor@hotmail.com>
2023-03-31 17:00:01 +0000

    adds two really large numbers

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,5 +1,16 @@
 #include <string.h>
 
+/**
+ * infinite_add - adds two numbers
+ *
+ * @n1: first array of numbers
+ * @n2: second array of numbers
+ * @r: buffer to store results
+ * @size_r: buffer size
+ *
+ * Return: pointer to results
+ */
+
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
 	int i, a, b, sum;
@@ -11,17 +22,14 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 	{
 		return (0);
 	}
-
 	r[maxlen] = '\0';
 	len1--;
 	len2--;
-
-	for (i = maxlen - 1; i >=0; i--)
+	for (i = maxlen - 1; i >= 0; i--)
 	{
 		a = len1 >= 0 ? n1[len1] - 48 : 0;
 		b = len2 >= 0 ? n2[len2] - 48 : 0;
 		sum = a + b + carry;
-
 		if (sum > 9)
 		{
 			carry = 1;
@@ -29,16 +37,13 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 		}
 		else
 			carry = 0;
-
 		r[i] = sum + '0';
-
 		len1--;
 		len2--;
 	}
-
 	if (carry)
 	{
-		if ((maxlen + 2 ) > size_r)
+		if ((maxlen + 2) > size_r)
 			return (0);
 		r[i + 1] = carry + '0';
 	}
