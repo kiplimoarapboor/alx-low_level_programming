65aa9de32f8a9927a119287dbe3e751a1b94a871
kiplimoboor <kiplimoboor@hotmail.com>
2023-03-31 05:13:52 +0000

    copies a string

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -3,7 +3,7 @@
  *
  * @dest: the destination pointer for the strin
  * @src: the source of the string
- * n: number of bytes to be copied from src
+ * @n: number of bytes to be copied from src
  *
  * Return: the destination pointer
  */
@@ -13,7 +13,7 @@ char *_strncpy(char *dest, char *src, int n)
 	int j;
 
 	for (j = 0; j < n && src[j] != '\0'; j++)
-		dest[j] = src [j];
+		dest[j] = src[j];
 
 	for (; j < n; j++)
 		dest[j] = '\0';
