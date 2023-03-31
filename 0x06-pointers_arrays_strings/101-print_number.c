b8ae84aae673ddae6b9440bb75a51875ba2c5c98
kiplimoboor <kiplimoboor@hotmail.com>
2023-03-31 11:11:55 +0000

    prints an integer using putchar

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -9,14 +9,16 @@
 
 void print_number(int n)
 {
+	unsigned int num = n;
+
 	if (n < 0)
 	{
 		_putchar('-');
-		n = -n;
+		num = -n;
 	}
 
-	if (n / 10)
-		print_number(n / 10);
+	if (num / 10)
+		print_number(num / 10);
 
-	_putchar(48 + n % 10);
+	_putchar(48 + num % 10);
 }
