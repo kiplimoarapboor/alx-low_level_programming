3d1897ac70ce54b07a77ba55243112026ec02c67
kiplimoboor <kiplimoboor@hotmail.com>
2023-03-31 20:53:49 +0000

    prints buffer

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -10,14 +10,13 @@
 void print_buffer(char *b, int size)
 {
 	int i, j, k;
-	int length = size / sizeof(char);
 
 	if (size <= 0)
 	{
 		printf("\n");
 		return;
 	}
-	for (i = 0; i <= length; i += 10)
+	for (i = 0; i <= size; i += 10)
 	{
 		printf("%08x:", i);
 
@@ -25,7 +24,7 @@ void print_buffer(char *b, int size)
 		{
 			if (j % 2 == 0)
 				printf(" ");
-			if (j < length)
+			if (j < size)
 				printf("%02x", b[j]);
 			else
 				printf("  ");
@@ -40,7 +39,6 @@ void print_buffer(char *b, int size)
 			if (b[k] < 32 || b[k] > 126)
 				b[k] = '.';
 			putchar(b[k]);
-
 		}
 
 		printf("\n");
