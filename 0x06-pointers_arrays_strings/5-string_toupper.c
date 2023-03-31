7a45ac0afb949dae0b28d45252db92ac999487bd
kiplimoboor <kiplimoboor@hotmail.com>
2023-03-31 07:19:07 +0000

    changes all lowercase letters of a string to uppercase

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -8,25 +8,13 @@
 
 char *string_toupper(char *c)
 {
-	int i = 0, len, diff;
+	int i = 0;
 
 	while (c[i])
-		i++;
-
-	len = i;
-
-	for (i = 0; i < len; i++)
 	{
-		if (c[i] >= 65 && c[i] <= 90)
-		{
-			diff = c[i] - 65;
-			c[i] = diff + 97;
-		}
 		if (c[i] >= 97 && c[i] <= 122)
-		{
-			diff = c[i] - 97;
-			c[i] = diff + 65;
-		}
+			c[i] -= 32;
+		i++;
 	}
 	return (c);
 }
