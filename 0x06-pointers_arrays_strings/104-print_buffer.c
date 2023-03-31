a1a9501fc4e8abb53eae9566ca44f0657c305548
kiplimoboor <kiplimoboor@hotmail.com>
2023-03-31 19:10:44 +0000

    prints a buffer

---
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+/**
+ * print_buffer- prints a buffer
+ *
+ * @b: pointer to the buffer
+ * @size: the size of the buffer to be printed
+ */
+
+void print_buffer(char *b, int size)
+{
+	int i, j, k;
+	int length = size / sizeof(char);
+
+	for (i = 0; i <= length; i += 10)
+	{
+		printf("%08x:", i);
+		for (j = i; j < i + 10; j++)
+		{
+			if (j < length)
+			{
+				if (j % 2 == 0)
+					printf(" ");
+				printf("%02x", b[j]);
+			}
+			else
+				printf("%*s", 10 - j  % 10, " ");
+			if (j % 10 == 9)
+				printf(" ");
+
+		}
+		for (k = i; k < i + 10; k++)
+		{
+			if (k < length)
+			{
+				if (b[k] < 32 || b[k] > 126)
+					b[k] = '.';
+				putchar(b[k]);
+			}
+		}
+		printf("\n");
+	}
+}
