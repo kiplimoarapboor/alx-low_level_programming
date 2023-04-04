5228d0afd08afe62fb7145514de661eafa2c87f2
kiplimoboor <kiplimoboor@hotmail.com>
2023-04-04 08:22:17 +0000

    hi

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -11,12 +11,12 @@
 
 char *_strstr(char *haystack, char *needle)
 {
-	int i = 0, j = 0;
+	int i = 0, j;
 
 	while (haystack[i])
 	{
 		j = 0;
-		while (needle[j] == haystack[i + j])
+		while (haystack[i + j] && needle[j] == haystack[i + j])
 		{
 			j++;
 			if (needle[j] == '\0')
