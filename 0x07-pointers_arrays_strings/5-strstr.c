419d75769eb5977054adfbcfea6fca867efbbc9c
kiplimoboor <kiplimoboor@hotmail.com>
2023-04-04 08:03:31 +0000

    locates a substring

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,29 @@
+#include <string.h>
+
+/**
+ * _strstr- locates a substring in string
+ *
+ * @haystack: the string
+ * @needle: the substring
+ *
+ * Return: if substring found, the address, NULL otherwise
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	int i = 0, j = 0;
+
+	while (haystack[i])
+	{
+		while (needle[j] == haystack[i + j])
+		{
+			if (needle[j] == '\0')
+				return (&haystack[i]);
+			j++;
+		}
+
+		i++;
+	}
+
+	return ('\0');
+}
