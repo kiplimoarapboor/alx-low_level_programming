2492558b0df5d79689848c3b8f7bfbb4a72e8e36
kiplimoboor <kiplimoboor@hotmail.com>
2023-04-11 23:41:35 +0000

    returns pointer to allocated space in memory

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,36 @@
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * _strdup-  returns a pointer to a newly allocated space in memory,
+ * which contains a copy of the string given as a parameter
+ *
+ * @str: the string
+ *
+ * Return: pointer to created memory
+ */
+
+char *_strdup(char *str)
+{
+	int i, len;
+	char *array;
+
+	if (str == NULL)
+		return (NULL);
+
+	len = strlen(str);
+
+	array = malloc(sizeof(char) * len);
+
+	if (array == NULL)
+		return (NULL);
+
+	for (i = 0; i < len; i++)
+	{
+		array[i] = str[i];
+	}
+
+	return (array);
+
+	free(array);
+}
