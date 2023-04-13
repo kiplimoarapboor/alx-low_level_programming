afccd4804b33696e24f79ca709e66a1614bcc88b
kiplimoboor <kiplimoboor@hotmail.com>
2023-04-13 21:14:52 +0000

    concatenates two strings

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -9,48 +9,29 @@
  *
  * Return: pointer to new string
  */
-size_t len(char *str);
-char *_memcpy(char *dest, char *src, unsigned int n);
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	size_t len1 = len(s1);
-	size_t len2 = len(s2);
-	size_t len = len1 + len2;
-	char *string = malloc(len + 1);
+	unsigned int len = 0, i;
+	char *string;
+
+	while (s1[len])
+		len++;
+
+	len += n;
+
+	string = malloc(len + 1);
 
 	if (string == NULL)
 		return (NULL);
 
-	if (len1 > 0)
-		_memcpy(string, s1, len1);
-	if (len2 > 0 && n > 0)
-		_memcpy(string + len1, s2, n);
+	for (len = 0; s1[len]; len++)
+		string[len] = s1[len];
 
-	string[len] = '\0';
+	for (i = 0; s2[i] && i < n; i++)
+		string[i + len] = s2[i];
+
+	string[len + i] = '\0';
 
 	return (string);
-}
-
-size_t len(char *str)
-{
-	size_t i = 0;
-	while (str[i])
-	{
-		i++;
-	}
-
-	return (i);
-}
-
-char *_memcpy(char *dest, char *src, unsigned int n)
-{
-	unsigned int i;
-
-	for (i = 0; i < n; i++)
-	{
-		dest[i] = src[i];
-	}
-
-	return (dest);
 }
