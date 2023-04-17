9d7d2403d2fa757836abaef4bc0f6f2583ecbfd0
kiplimoboor <kiplimoboor@hotmail.com>
2023-04-17 10:22:24 +0000

    creates new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -16,32 +16,31 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *new_dog;
 
-
-	if (name == && owner == NULL)
-		return (NULL);
-
-	new_dog = malloc(sizeof(dog_t));
-	if (new_dog == NULL)
-		return (NULL);
-
-	new_dog->name = malloc(strlen(name));
-	if (new_dog->name == NULL)
+	if (name && owner)
 	{
-		free(new_dog);
-		return (NULL);
-	}
+		new_dog = malloc(sizeof(dog_t));
+		if (new_dog == NULL)
+			return (NULL);
 
-	new_dog->owner = malloc(strlen(owner));
-	if (new_dog->owner == NULL)
-	{
-		free(new_dog->name);
-		free(new_dog);
-		return (NULL);
-	}
+		new_dog->name = malloc(strlen(name));
+		if (new_dog->name == NULL)
+		{
+			free(new_dog);
+			return (NULL);
+		}
 
-	new_dog->name = strcpy(new_dog->name, name);
-	new_dog->owner = strcpy(new_dog->owner, owner);
-	new_dog->age = age;
+		new_dog->owner = malloc(strlen(owner));
+		if (new_dog->owner == NULL)
+		{
+			free(new_dog->name);
+			free(new_dog);
+			return (NULL);
+		}
+
+		new_dog->name = strcpy(new_dog->name, name);
+		new_dog->owner = strcpy(new_dog->owner, owner);
+		new_dog->age = age;
+	}
 
 	return (new_dog);
 }
