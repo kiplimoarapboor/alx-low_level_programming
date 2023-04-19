c495d6ed036985597bd5a465c68b5796969e61e4
kiplimoboor <kiplimoboor@hotmail.com>
2023-04-19 15:45:54 +0000

    performs simple operations

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -12,7 +12,7 @@
 
 int (*get_op_func(char *s))(int a, int b)
 {
-	int i;
+	int i = 0;
 	op_t ops[] = {
 		{"+", op_add},
 		{"-", op_sub},
@@ -22,12 +22,11 @@ int (*get_op_func(char *s))(int a, int b)
 		{NULL, NULL}
 	};
 
-	for (i = 0; ops[i].op; i++)
+	while (ops[i].op)
 	{
 		if (strcmp(s, ops[i].op) == 0)
-		{
 			return (ops[i].f);
-		}
+		i++;
 	}
 
 	return (NULL);
