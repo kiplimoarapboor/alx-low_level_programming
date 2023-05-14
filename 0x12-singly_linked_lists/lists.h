1b66a2d1857c0be9162b8369c67cd4647988e9dc
kiplimoboor <kiplimoboor@hotmail.com>
2023-05-14 20:37:51 +0000

    main header file

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,6 +1,11 @@
 #ifndef LIST_H
 #define LIST_H
 
+#include <stddef.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
