b74ed2ec473e29a9df831bbbf628f86e7200d0fa
kiplimoboor <kiplimoboor@hotmail.com>
2023-05-16 17:58:06 +0000

    inserts new node

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -16,7 +16,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *new = malloc(sizeof(listint_t));
 	listint_t *temp = (*head);
 
-	if (!new)
+	if (!new || !*head)
 		return (NULL);
 
 	length = listint_len(*head);
