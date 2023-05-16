d3600b05fc67d74c5a4dbeafa201a4a4dcbb93f7
kiplimoboor <kiplimoboor@hotmail.com>
2023-05-16 18:14:16 +0000

    inserts node at nth position

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -16,31 +16,30 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *new = malloc(sizeof(listint_t));
 	listint_t *temp = (*head);
 
-	if (!new || !*head)
+	if (!new)
 		return (NULL);
 
-	length = listint_len(*head);
+	length = listint_len((*head));
 
 	if (idx > length)
 		return (NULL);
 
 	new->n = n;
-	new->next = NULL;
 
 	if (idx == 0)
 	{
-		new->next = *head;
-		*head  = new;
+		new->next = (*head);
+		(*head) = new;
 	}
 	else
 	{
 		while (--idx)
 			temp = temp->next;
+
 		new->next = temp->next;
 		temp->next = new;
 	}
-
-	return (*head);
+	return (new);
 }
 
 /**
