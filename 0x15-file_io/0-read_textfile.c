2f51446576ab5b26f1dc55fb63c946ac997c6ab5
kiplimoboor <kiplimoboor@hotmail.com>
2023-05-23 05:23:16 +0000

    reads and prints file to POSIX

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * read_textfile- reads a text file and prints it
+ *
+ * @filename: filename
+ * @letters: number of characters to print
+ *
+ * Return: number of letters in file
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	FILE *fp;
+	char c;
+	unsigned int count = 0;
+
+	fp = fopen(filename, "r");
+	if (!fp)
+		return (0);
+
+	while ((c = fgetc(fp)) != EOF && count < letters)
+	{
+		write(1, &c, 1);
+		count++;
+	}
+
+	fclose(fp);
+
+	return (count);
+}
