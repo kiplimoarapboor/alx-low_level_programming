406874fa072bfc6a16c455a35ffcee05f9cec138
kiplimoboor <kiplimoboor@hotmail.com>
2023-05-23 05:33:06 +0000

    reads and prints file characters

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -13,17 +13,21 @@ ssize_t read_textfile(const char *filename, size_t letters)
 {
 	FILE *fp;
 	char c;
+	int flag;
 	unsigned int count = 0;
+
+	if (!filename)
+		return (0);
 
 	fp = fopen(filename, "r");
 	if (!fp)
 		return (0);
 
-	if (!filename)
-		return (0);
 	while ((c = fgetc(fp)) != EOF && count < letters)
 	{
-		write(1, &c, 1);
+		flag = write(1, &c, 1);
+		if (flag == -1)
+			return (0);
 		count++;
 	}
 
